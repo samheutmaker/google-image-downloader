5a1e2ec378037524eb5266f6864c9757bf8e4a46
SamHeutmaker <samheutmaker@gmail.com>
2018-05-16 13:21:07 -0700

    Add image search and downloading

---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require('fs-extra-promise');
+const request = require('request-promise');
+const IMAGE_SAVE_BASE_PATH = './images/';
+var Scraper = require('images-scraper'),
+  google = new Scraper.Google();
+
+function downloadImage(url, filePath) {
+  let urlCopy = url;
+  let saveFilePath = `${IMAGE_SAVE_BASE_PATH}${filePath}`;
+  filePath = `${IMAGE_SAVE_BASE_PATH}${filePath.includes('/') ? filePath.split('/')[0] : ''}`;
+  return ensureDir(filePath)
+    .then(() => request.head(url))
+    .then(() => {
+      return new Promise((resolve) => {
+        request(url).pipe(fs.createWriteStream(saveFilePath)).on('close', () => resolve());
+      });
+    });
+}
+
+function ensureDir(dir) {
+  return new Promise((resolve, reject) => {
+    fs.ensureDir(dir, error => error ? reject(error) : resolve());
+  });
+}
+
+function searchGoogle(search) {
+  return google.list({
+      keyword: search.keyword,
+      num: 10,
+      detail: true,
+      nightmare: {
+        show: true
+      }
+    })
+    .then(function(res) {
+      search.images = res;
+      return search;
+    }).catch(function(err) {
+      console.log('err', err);
+    });
+}
+
+let searches = [
+  {
+    keyword: 'american currency',
+    name: 'american',
+    saveAs: (name, index) => `${name}/${name}.${index}.jpg`,
+    images: []
+  },
+  {
+    keyword: 'canadian currency',
+    name: 'canadian',
+    saveAs: (name, index) => `${name}/${name}.${index}.jpg`,
+    images: []
+  }
+];
+
+Promise.all(searches.map(search => searchGoogle(search)))
+  .then(res => {
+    console.log(res);
+    return Promise.all(res.map(search => Promise.all(search.images.map((image, index) => downloadImage(image, search.saveAs(search.name, index))))));
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(error => console.error(error));
